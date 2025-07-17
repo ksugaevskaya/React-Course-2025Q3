d554bb7215d0f1a7caacbfc9757e66e1f74ceda6
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-07-17 23:50:44 +0300

    feat: add testing infrastructure

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, type UserConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/React-Course-2025Q3/class-components/',
-});
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'lcov'],
+      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+
+      threshold: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+  },
+} as UserConfig);
