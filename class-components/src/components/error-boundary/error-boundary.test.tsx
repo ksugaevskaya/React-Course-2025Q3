266c9517f1f985e69fad5b38125093512cdff189
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-07-19 23:18:30 +0300

    feat: add error-boundary testing

---
+++ class-components/src/components/error-boundary/error-boundary.test.tsx
@@ -0,0 +1,46 @@
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import ErrorBoundary from './error-boundary';
+
+import { afterEach, expect, test } from 'vitest';
+import { Component, type ReactNode } from 'react';
+
+afterEach(cleanup);
+
+test('check renderring children if no error', () => {
+  render(
+    <ErrorBoundary>
+      <h1>Hello, world</h1>
+    </ErrorBoundary>
+  );
+
+  expect(screen.queryByText('Hello, world')).not.toBeNull();
+  expect(screen).toMatchSnapshot();
+});
+
+class ErrorComponent extends Component {
+  render(): ReactNode {
+    throw new Error('test error');
+
+    return null;
+  }
+}
+
+test('check showing error if children have error', () => {
+  render(
+    <ErrorBoundary>
+      <ErrorComponent></ErrorComponent>
+    </ErrorBoundary>
+  );
+
+  expect(screen.queryByText('Oops... Something went wrong...')).not.toBeNull();
+});
+
+test('check if error button was clicked', () => {
+  render(
+    <ErrorBoundary>
+      <ErrorComponent></ErrorComponent>
+    </ErrorBoundary>
+  );
+
+  fireEvent.click(screen.getByText('Try again'));
+});
