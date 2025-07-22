f9228f4020bf0d4135e2d602088a49ed67dbf4d0
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-07-22 23:54:33 +0300

    feat: add routing setup

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,15 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css';
 import App from './components/app/App.tsx';
 import ErrorBoundary from './components/error-boundary/error-boundary.tsx';
+import About from './pages/about-component.tsx';
+import NotFound from './pages/not-found-component.tsx';
 
 const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
     <StrictMode>
       <ErrorBoundary>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </ErrorBoundary>
     </StrictMode>
   );
