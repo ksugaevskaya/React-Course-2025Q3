a53fbf71e19ab12517c7bf921162f6de70fff384
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-07-23 22:42:10 +0300

    feat: add about page layout & css styles

--- class-components/src/pages/about-component.tsx
+++ class-components/src/pages/about-component.tsx
@@ -1,3 +1,30 @@
+import RSlogo from '../assets/rss-logo.svg';
+import KG from '../assets/KG.png';
+import './about.css';
+
 export default function About() {
-  return <div> About </div>;
+  return (
+    <>
+      <div className="card-container">
+        <div>
+          <img className="KG-image" src={KG} />
+        </div>
+        <div className="text-container">
+          <div className="name">Ksenia Gaevskaya </div>
+
+          <div className="text">
+            I am a Frontend Engineer with a passion for crafting intuitive,
+            high-quality user interfaces. A precise learner with a sharp eye for
+            detail, loving solving complex problems, and seeing the immediate
+            impact of well-executed code. I am driven by the challenge of
+            turning ideas into responsive, performant, and visually compelling
+            applications.
+          </div>
+        </div>
+      </div>
+      <div className="logo">
+        <img src={RSlogo} />
+      </div>
+    </>
+  );
 }
