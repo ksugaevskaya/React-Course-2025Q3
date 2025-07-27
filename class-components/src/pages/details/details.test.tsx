eff066ca6ee6a54de615dbbb4ea4eb0c5b504369
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-07-27 22:42:38 +0200

    feat: add tests to details component

--- class-components/src/pages/details/details.test.tsx
+++ class-components/src/pages/details/details.test.tsx
@@ -1,7 +1,14 @@
 import '@testing-library/jest-dom';
-import { cleanup, render, screen } from '@testing-library/react';
-import { Details } from './details';
-import { afterEach, expect, test } from 'vitest';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
+import DetailsPage, { Details } from './details';
+import { afterEach, expect, test, vitest } from 'vitest';
+import { MemoryRouter } from 'react-router';
 
 afterEach(cleanup);
 
@@ -25,4 +32,53 @@ test('check detail component rendering', () => {
   expect(screen.queryByText('Weight: 20')).not.toBeNull();
   expect(screen.queryByText('Height: 20')).not.toBeNull();
   expect(screen.queryByText('Types: electricity')).not.toBeNull();
+});
+
+let mockedFetchPokemonFn = vitest.fn();
+
+vitest.mock('../../api/api', () => ({
+  fetchPokemonById: () => mockedFetchPokemonFn(),
+}));
+
+test('check page rendering', async () => {
+  mockedFetchPokemonFn.mockResolvedValue({
+    name: 'Raichu',
+    description:
+      'Its long tail serves as a ground to protect itself from its own high-voltage power',
+    image: 'https:/google.com',
+    experience: 218,
+    weight: 300,
+    height: 8,
+    types: 'electric',
+  });
+  const { container } = render(
+    <MemoryRouter>
+      <DetailsPage></DetailsPage>
+    </MemoryRouter>
+  );
+
+  expect(container).toMatchSnapshot();
+  await waitFor(() => {
+    expect(screen.queryByText('Close')).not.toBeNull();
+  });
+  expect(screen.queryByText('RAICHU')).not.toBeNull();
+  expect(
+    screen.queryByText(
+      'Its long tail serves as a ground to protect itself from its own high-voltage power'
+    )
+  ).not.toBeNull();
+  expect(screen.queryByText('Base experience: 218')).not.toBeNull();
+  expect(screen.queryByText('Weight: 300')).not.toBeNull();
+  expect(screen.queryByText('Height: 8')).not.toBeNull();
+  expect(screen.queryByText('Types: electric')).not.toBeNull();
+});
+
+test('check if the close button clicked', async () => {
+  render(
+    <MemoryRouter>
+      <DetailsPage></DetailsPage>
+    </MemoryRouter>
+  );
+  await waitFor(() => expect(screen.queryByText('Close')).not.toBeNull());
+  fireEvent.click(screen.getByText('Close'));
 });
