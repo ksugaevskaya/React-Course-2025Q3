c5d45b25f59d53c5c50981997387c0bf8341f4fa
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-08-17 21:43:03 +0200

    fix`; all pages redirect to 404

--- class-components/next.config.mjs
+++ class-components/next.config.mjs
@@ -8,14 +8,6 @@ const nextConfig = {
       new URL('https://raw.githubusercontent.com/PokeAPI/sprites/**'),
     ],
   },
-  pageExtensions: [
-    'page.tsx',
-    'page.ts',
-    'page.jsx',
-    'page.js',
-    'api.ts',
-    'api.js',
-  ],
 };
 
 const withNextIntl = createNextIntlPlugin();
