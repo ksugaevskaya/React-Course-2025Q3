b535359469b8a6a0d53dc27adcd91fc7f6fe6459
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-08-17 23:13:48 +0200

    fix: details hard route

---
+++ class-components/src/app/[locale]/[id]/page.tsx
@@ -0,0 +1,7 @@
+import DetailsPage from '../../../pages/details/details';
+
+type Props = { params: { id: string } };
+
+export default async function PokemonDetails({ params }: Props) {
+  return <DetailsPage pokemonId={params.id} />;
+}
