44c50ac2382fb747f6ce9f4ff97c0baa9b9fe98b
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-08-17 22:25:38 +0200

    fix: 404 triggering error

--- class-components/src/app/not-found.tsx
+++ class-components/src/app/not-found.tsx
@@ -1,11 +1,5 @@
 import NotFound from '../pages/not-found/not-found-component';
 
 export default function Error404() {
-  return (
-    <html>
-      <body>
-        <NotFound />
-      </body>
-    </html>
-  );
+  return <NotFound />;
 }
