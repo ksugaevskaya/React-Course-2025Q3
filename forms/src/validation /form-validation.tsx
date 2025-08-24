20dce32f0cd2d68813193a6230bc604203e1d872
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-08-24 18:10:43 +0200

    refactor: move validation schema to separate folder

---
+++ forms/src/validation /form-validation.tsx
@@ -0,0 +1,71 @@
+import * as yup from 'yup';
+
+export const schema = yup
+  .object({
+    fname: yup
+      .string()
+      .test(
+        'firstLetterCapitalized',
+        'The first letter should be capitalized',
+        (value) =>
+          value?.[0] === value?.[0]?.toUpperCase?.() &&
+          !'0123456789'.includes(value[0])
+      )
+      .required('First name must not be empty!'),
+    age: yup
+      .number()
+      .typeError('Age must be a number')
+      .positive('Age should be a positive number')
+      .integer('Age should be a number')
+      .required('Age must not be empty!'),
+    email: yup
+      .string()
+      .email(
+        'Email address must be properly formatted (e.g., user@example.com)'
+      )
+      .required('Email must not be empty'),
+    password: yup
+      .string()
+      .required('Password is required')
+      .matches(
+        /[a-z]/,
+        'Password must contain at least one lowercase letter (a-z).'
+      )
+      .matches(
+        /[A-Z]/,
+        'Password must contain at least one uppercase letter (A-Z).'
+      )
+      .matches(/\d/, 'Password must contain at least one digit')
+      .matches(
+        /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/,
+        `Password must contain at least one special character !"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~'`
+      ),
+    passwordRepeat: yup
+      .string()
+      .required('Repeat password is required')
+      .oneOf(
+        [yup.ref('password')],
+        'Repeat password should match current password'
+      ),
+    gender: yup.string().required('Gender should be selected'),
+    checkbox: yup
+      .string()
+      .oneOf(['true'], 'Please accept terms and conditions agreement'),
+    file: yup
+      .mixed()
+      .required('A file is required')
+      .test(
+        'fileSize',
+        'File too large, must be less than 1MB',
+        (value) => !value || (value && value[0].size <= 1_000_000)
+      )
+      .test(
+        'fileFormat',
+        'Unsupported Format, only JPEG or PNG',
+        (value) =>
+          !value ||
+          (value && value[0].type === 'image/jpeg') ||
+          value[0].type === 'image/png'
+      ),
+  })
+  .required();
