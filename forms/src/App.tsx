d29216d298db01632d1975e54d12b93c7dbf3695
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-08-24 20:51:43 +0200

    feat: add data display after form submission

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -28,11 +28,13 @@ function App() {
   };
   return (
     <>
-      <button onClick={handleControlledButton}> Open Controlled Modal</button>
-      <button onClick={handleUncontrolledButton}>
-        {' '}
-        Open Uncontrolled Modal
-      </button>
+      <div className="button-container">
+        <button onClick={handleControlledButton}> Open Controlled Modal</button>
+        <button onClick={handleUncontrolledButton}>
+          {' '}
+          Open Uncontrolled Modal
+        </button>
+      </div>
       <Modal visible={controlled} onClose={handleCloseButtonControlled}>
         <ControlledForm></ControlledForm>
       </Modal>
@@ -41,11 +43,89 @@ function App() {
       </Modal>
       <div>
         <p>Controlled:</p>
-        <p>{JSON.stringify(form.controlled, null, 2)}</p>
+        <div className="data-container">
+          <div className="text-container">
+            <div>
+              {' '}
+              <b>NAME :</b> {form.controlled?.fname}
+            </div>
+            <div>
+              <b>AGE </b> {form.controlled?.age}
+            </div>
+            <div>
+              <b>EMAIL: </b>
+              {form.controlled?.email}
+            </div>
+            <div>
+              <b>PASSWORD:</b> {form.controlled?.password}
+            </div>
+            <div>
+              {' '}
+              <b>REPEAT PASSWORD: </b>
+              {form.controlled?.passwordRepeat}
+            </div>
+            <div>
+              {' '}
+              <b>GENDER: </b>
+              {form.controlled?.gender}
+            </div>
+            <div>
+              {' '}
+              <b>T&C: </b>
+              {form.controlled?.checkbox}
+            </div>{' '}
+          </div>
+          <div>
+            <img
+              src={form.controlled?.file}
+              alt="Base64 image"
+              style={{ width: 200, height: 200 }}
+            />
+          </div>
+        </div>
       </div>
       <div>
         <p>Uncontrolled:</p>
-        <p>{JSON.stringify(form.uncontrolled, null, 2)}</p>
+        <div className="data-container">
+          <div className="text-container">
+            <div>
+              {' '}
+              <b>NAME :</b> {form.uncontrolled?.fname}
+            </div>
+            <div>
+              <b>AGE </b> {form.uncontrolled?.age}
+            </div>
+            <div>
+              <b>EMAIL: </b>
+              {form.uncontrolled?.email}
+            </div>
+            <div>
+              <b>PASSWORD:</b> {form.uncontrolled?.password}
+            </div>
+            <div>
+              {' '}
+              <b>REPEAT PASSWORD: </b>
+              {form.uncontrolled?.passwordRepeat}
+            </div>
+            <div>
+              {' '}
+              <b>GENDER: </b>
+              {form.uncontrolled?.gender}
+            </div>
+            <div>
+              {' '}
+              <b>T&C: </b>
+              {form.uncontrolled?.checkbox}
+            </div>{' '}
+          </div>
+          <div>
+            <img
+              src={form.uncontrolled?.file}
+              alt="Base64 image"
+              style={{ width: 200, height: 200 }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
