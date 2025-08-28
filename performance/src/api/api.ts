5cf583529da44a18aa1a7686504bf65d93004599
ksugaevskaya <ksu.gaevskaya@gmail.com>
2025-08-28 23:43:56 +0200

    feat: add api functionality

---
+++ performance/src/api/api.ts
@@ -0,0 +1,54 @@
+const OWID_CO2_JSON =
+  'https://nyc3.digitaloceanspaces.com/owid-public/data/co2/owid-co2-data.json';
+
+type YearDatum = {
+  year: number;
+  population?: number;
+  co2?: number;
+  co2_per_capita?: number;
+};
+
+type Entity = {
+  iso_code: string;
+  country?: string;
+  name?: string;
+  data: YearDatum[];
+};
+
+type DataMap = Record<string, Entity>;
+
+export type Row = {
+  name: string;
+  iso: string;
+  year: number;
+  population?: number;
+  co2?: number;
+  co2PerCapita?: number;
+};
+
+export async function getCO2RowsLatest(): Promise<Row[]> {
+  const res = await fetch(OWID_CO2_JSON);
+  const json = (await res.json()) as DataMap;
+
+  const rows: Row[] = [];
+  for (const key in json) {
+    const ent = json[key];
+
+    if (!ent?.iso_code) continue;
+
+    const last = ent.data?.[ent.data.length - 1];
+    if (!last) continue;
+
+    rows.push({
+      name: ent.country ?? ent.name ?? key,
+      iso: ent.iso_code,
+      year: last.year,
+      population: last.population,
+      co2: last.co2,
+      co2PerCapita: last.co2_per_capita,
+    });
+  }
+
+  rows.sort((a, b) => a.name.localeCompare(b.name));
+  return rows;
+}
